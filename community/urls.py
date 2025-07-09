9ca1c0728b1aadbf35d84384a91dbd3f8d3f34cb
caddy <caddycarine@gmail.com>
2025-07-09 03:10:18 -0600

    Project init

---
+++ community/urls.py
@@ -0,0 +1,17 @@
+from django.urls import path, include
+from rest_framework.routers import DefaultRouter
+from .views import UserViewSet, AnnouncementViewSet, MessageViewSet, members
+
+router = DefaultRouter()
+router.register(r'users', UserViewSet, basename='user')
+router.register(r'announcements', AnnouncementViewSet, basename='announcement')
+router.register(r'messages', MessageViewSet, basename='message')
+
+urlpatterns = [
+    path('members/', members, name='members'),
+    path('', include(router.urls)),
+    # You might want to add a login/logout endpoint here for session authentication
+    # For token authentication, you'd typically use rest_framework.authtoken.views.obtain_auth_token
+    path('auth/', include('rest_framework.urls', namespace='rest_framework')), # For browsable API login/logout
+    #path('auth/token/', include('rest_framework.authtoken.urls')), # For obtaining auth tokens
+]
